9550e576ee1484229c1340bff2a2b4deedc91ee6
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2025-11-26 17:38:37 +0200

    Core-logic, testproblems
    - Added Mishra's constrained function (single-objective).

--- desdeo/problem/testproblems/single_objective.py
+++ desdeo/problem/testproblems/single_objective.py
@@ -110,3 +110,56 @@ def new_branin_function() -> Problem:
         objectives=[f_1],
         constraints=[c_1],
     )
+
+
+def mishras_bird_constrained() -> Problem:
+    """Implements the constrained variant of Mishra's bird function.
+
+    Global optima: -106.7645367 at [-3.1302468, -1.5821422]
+    """
+    x_1 = Variable(
+        name="x_1",
+        symbol="x_1",
+        variable_type=VariableTypeEnum.real,
+        lowerbound=-10.0,
+        upperbound=0.0,
+        initial_value=-0.1,
+    )
+    x_2 = Variable(
+        name="x_2",
+        symbol="x_2",
+        variable_type=VariableTypeEnum.real,
+        lowerbound=-6.5,
+        upperbound=0.0,
+        initial_value=-0.1,
+    )
+
+    f_1_def = "Sin(x_2)*Exp((1 - Cos(x_1))**2) + Cos(x_1)*Exp((1 - Sin(x_2))**2) + (x_1 - x_2)**2"
+    f_1 = Objective(
+        name="f_1",
+        symbol="f_1",
+        func=f_1_def,
+        maximize=False,
+        is_linear=False,
+        is_convex=False,
+        is_twice_differentiable=True,
+    )
+
+    c_1_def = "(x_1 + 5)**2 + (x_2 + 5)**2 - 25"
+    c_1 = Constraint(
+        name="c_1",
+        symbol="c_1",
+        cons_type=ConstraintTypeEnum.LTE,
+        func=c_1_def,
+        is_linear=False,
+        is_convex=False,
+        is_twice_differentiable=True,
+    )
+
+    return Problem(
+        name="Mishra's bird function",
+        description="The constrained variant of Mishra's bird function",
+        variables=[x_1, x_2],
+        objectives=[f_1],
+        constraints=[c_1],
+    )
